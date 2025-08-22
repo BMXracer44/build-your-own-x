8df5da4d6714e206f834f441289a85a6f870474b
BMXracer44 <fergusalex444@gmail.com>
2025-08-22 12:32:30 -0500

    Starting pseudo-code

---
+++ Database/main.cpp
@@ -0,0 +1,25 @@
+
+//Listening socket & connection socket pseudo code
+/*
+ * fd = socket() obtain a socket handle
+ * bind(fd, address) Set listening IP port 
+ * listen(fd) Create listening socket 
+ * while(true){
+ *  conn_fd = accept(fd)
+ *  do_something_with(conn_fd)
+ *  close(conn_fd)
+ * }
+ * 
+ * Then use the accept() API to wait for incoming TCP connections
+ */
+
+//Connection from a client pseudo-code
+/* 
+ * fd = socket()
+ * connect(fd, address)
+ * do_something_with(fd)
+ * close(fd)
+ *
+ */ 
+
+
